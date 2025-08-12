82bf44da1732f426ea2930782084b423628a1162
nikhil kumar <nikhilpanothra687@gmail.com>
2025-08-13 02:53:12 +0530

    changes

--- Frontend/src/services/api.js
+++ Frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_BASE_URL
+  baseURL: "https://full-stack-assessment-greencart.vercel.app/api"
 });
 
 // Add JWT token to every request if exists
